09beabe23acb1529dfd92c5f3b07629dfe4e781a
Mohammad Hayajneh <35841393+mohammadhayajneh@users.noreply.github.com>
2018-02-17 09:19:32 -0500

    Add files via upload

---
+++ celegans_phenotypes.py
@@ -0,0 +1,14 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> celegans_phenotypes = ['Emb', 'Him', 'Unc', 'Lon', 'Dpy', 'Sma']
+>>> for type in celegans_phenotypes:
+	print(type)
+
+	
+Emb
+Him
+Unc
+Lon
+Dpy
+Sma
+>>> 
